84bc6183877cf9e373243d257a532bfd272122c0
Danil Krivobok <krivoboc.krivoboc@gmail.com>
2025-05-13 07:34:41 +0300

    display update

--- system/display.py
+++ system/display.py
@@ -1,11 +1,15 @@
-
+import ctypes
 
 class DisplayManager:
     def __init__(self, device):
-        self.device = device
+        self.lib = ctypes.CDLL("./cpp/display/libst7735.so")
 
+        # Определяем типы для initialize_display()
+        self.lib.initialize_display.argtypes = []  
+        self.lib.initialize_display.restype = None
+        # Инициализация дисплея при запуске менеджера
+        self.lib.initialize_display()
 
-    async def show(self, menu):
-        pass
-
-    #GPIO инициализвация
+    
+    def draw(self, current_context):
+        data = current_context.get_draw_data()
